e3c68770a1c21873b92d4b13a307c3c8c88ff28e
nikakaladze <nikakaladze89@gmail.com>
2025-05-08 18:03:34 +0400

    first Commit

--- Compound-Calculator/main.c
+++ Compound-Calculator/main.c
@@ -4,29 +4,8 @@
 
 int main()
 {
-   double P;
-   double r;
-   double A;
-    int n;
-    int t;
+    
+    return 0;
 
-
-    printf("Enter your begining price (P): ");
-    scanf("%lf", &P);
-
-    printf("Enter the annual interest rate in %% (example. 8 %%): ");
-    scanf("%lf", &r);
-    r = r / 100.0;
-
-    printf("Enter the number of accruals per year. (n): ");
-    scanf("%d", &n);
-
-    printf("Enter the investment duration in years. (t): ");
-    scanf("%d", &t);
-
-    A = P * pow(1 + r / n, n * t);
-
-    // შედეგის გამოტანა
-    printf("The final amount will be: %.2f gel\n", A);
 
 }
