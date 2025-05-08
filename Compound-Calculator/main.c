0a9e83fda5f175cc2cc4184a294f5c81f0e19e7b
nikakaladze <nikakaladze89@gmail.com>
2025-05-08 19:20:59 +0400

    Finish Compound Calculator

--- Compound-Calculator/main.c
+++ Compound-Calculator/main.c
@@ -16,6 +16,16 @@ int main()
     printf("Enter the interest rate % (r): ");
     scanf("%f",&rate);
     rate = rate / 100;
+    printf("Enter the # of years (t): ");
+    scanf("%lf",&years);
+    printf("Enter # times compound per year (n): ");
+    scanf("%d",&timeCompounded);
+    total = principal *pow(1 + rate / timeCompounded,timeCompounded * years);
+    printf("after  %d years,the total will be $%.3lf",years,total);
+    
+
+
+
 
 
     
